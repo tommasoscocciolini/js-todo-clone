0da838f114896cd76469541aaebaab3a48eb2a66
tommaso <scocciolinitommaso@gmail.com>
2021-03-17 18:55:37 +0100

    aggiunta di todo con tasto invio

--- main.js
+++ main.js
@@ -33,4 +33,39 @@ $(document).ready(function () {
     $("#toDoList").append(template);
   })
 
-})
+  $("#titoloToDo").keyup(function(e){
+    if (e.which == 13) {
+      var newtitle = $("#titoloToDo").val();
+      $("#titoloToDo").val("");
+
+      var template = $("#todo li").clone();
+      template.children('span.titolo').text(newtitle)
+
+      template.children('span.done').click(function(){
+        $(this).parent().remove();
+      });
+
+      template.children('span.edit').click(function(){
+        var titoloDOM = $(this).siblings('span.titolo');
+        var titolo = titoloDOM.text();
+        titoloDOM.hide();
+
+        $(this).siblings('input').val(titolo).show();
+      });
+
+      template.children('input').keyup(function(e){
+        if (e.which == 13) {
+          var titolo = $(this).val();
+          $(this).hide();
+          $(this).siblings('span.titolo').text(titolo).show();
+        }
+      })
+
+      $("#toDoList").append(template);
+    }
+
+  })
+
+  })
+
+  
